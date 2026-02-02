bc6cb099613811ab6ee75eaf3a7f2175b3ab1d7a
yybeee <warsiahaan12.com>
2026-02-02 21:17:46 +0700

    Fix RoleMiddleware auth methods

--- app/Http/middleware/RoleMiddleware.php
+++ app/Http/middleware/RoleMiddleware.php
@@ -4,16 +4,11 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class RoleMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next, ...$roles): Response
     {
         if (!Auth::check()) {
@@ -23,7 +18,7 @@ class RoleMiddleware
         $userRole = Auth::user()->role;
 
         if (!in_array($userRole, $roles)) {
-            abort(403, 'Unauthorized access.');
+            abort(403, 'Unauthorized action.');
         }
 
         return $next($request);
