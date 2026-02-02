16644c5a8fb241b4d7d5e00bfa986417911117f9
yybeee <warsiahaan12.com>
2026-02-02 22:56:32 +0700

    Add location column to reports table

--- database/migrations/2026_02_02_225514_add_location_to_reports_table.php
+++ database/migrations/2026_02_02_225514_add_location_to_reports_table.php
@@ -6,17 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::table('reports', function (Blueprint $table) {
-            $table->string('location')->after('auditor_id');
+            //
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::table('reports', function (Blueprint $table) {
-            $table->dropColumn('location');
+            //
         });
     }
 };
