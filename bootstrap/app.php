fa010deca77dd8011ea9976b8a5c10804389b44c
yybeee <warsiahaan12.com>
2026-02-02 17:16:31 +0700

    feat: tambah RoleMiddleware untuk authorization

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // REGISTER MIDDLEWARE ROLE DI SINI
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
